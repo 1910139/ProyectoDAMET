0eaecfd0ce79d0903dbad2fbe5221306ed051fe9
franc <franc@Francisco>
2024-04-04 13:32:19 +0200

    inicio total

---
+++ src/main/java/com/mycompany/entornost4e2/EntornosT4E2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.entornost4e2;
+
+/**
+ *
+ * @author franc
+ */
+public class EntornosT4E2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
